292276ea8b393caffddf60c90221e27e3241b41a
Meghnad Saha <meghnadsaha422@gmail.com>
2024-03-18 23:58:27 +0530

    Delete Interview Assignments/Publicis Sapient Hiring Assessment/productCatalogueSearch directory

--- Interview Assignments/Publicis Sapient Hiring Assessment/productCatalogueSearch/src/main/java/com/sapient/productCatalogueSearch/listener/SupplierChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.sapient.productCatalogueSearch.listener;
-
-import com.sapient.productCatalogueSearch.domain.Supplier;
-
-public interface SupplierChangeListener {
-    void onSupplierChange ( Supplier supplier );
-}
